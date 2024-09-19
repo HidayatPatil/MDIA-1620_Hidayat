6c038dddd63e412c6dd1b038f921d0a70c2e0bd2
HidayatPatil <hpatil2@my.bcit.ca>
2024-09-19 15:43:40 -0700

    Add files via upload

---
+++ stables.js
@@ -0,0 +1,8 @@
+console.log("horses are great!") //string sample, it is a sequence of characters
+let horseName = "Gilbert"; //The variable is named using camel convention
+let horseAge = 5; //this is a number, it is a sequence of numbers. anything inside a "---" is a string
+let isHorseInStable = true; //This is a boolean
+//decleration "let" is only supposed to be used 1 / the first time when your declaring a variable
+console.log(horseName);
+console.log(horseAge);
+console.log(isHorseInStable);
